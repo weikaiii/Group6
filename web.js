9f86696ebf664921460a2590dfa79ac8378d26a7
weikaiii <30693906+weikaiii@users.noreply.github.com>
2017-11-13 22:23:55 +0800

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Mr. Hassan, we are from group 6 <br><br>Our group member are: <br> 3. Kho Kheng Eian \t CB15173<6> Lim Jing Jie \t CB15170');
+  res.send('Hello Mr. Hassan, we are from group 6 <br><br>Our group member are: <br> 1. Hii Wei Kai \t CB14155<br> 3. Kho Kheng Eian \t CB15173<br> 6. Lim Jing Jie \t CB15170');
 });
 
 app.listen(process.env.PORT || 3000);
