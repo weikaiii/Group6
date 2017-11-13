6f063cda04528e1f537ac962c76fb0d26ebe7116
Zahra SE <33603802+zahra-se93@users.noreply.github.com>
2017-11-13 23:09:03 +0800

    Update build.js

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Mr. Hassan, this is build engineering environment');
+  res.send('Hello Mr. Hassan, this is build engineering environment.The tools that can be use are git,bracket as editor');
 });
 
 app.listen(process.env.PORT || 3000);
